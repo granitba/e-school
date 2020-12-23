60968ab8e786cffab2ca7acdaecd45d18fe40b95
granitba <granittiti@gmail.com>
2020-12-23 22:54:50 +0100

    Shtohen directories assets/ dhe includes/, dhe file login.php

---
+++ includes/head.php
@@ -0,0 +1,5 @@
+<title>E-School</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="icon" href="logo.svg" type="image/x-icon">
+<link rel="stylesheet"  href="assets/css/main.css">
---
+++ login.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php include_once('includes/head.php'); ?>
+</head>
+<body id="login-body">
+<div id="login">
+    <p class="sign" align="center">Sign in</p>
+    <form class="login-form">
+        <input class="text-input" type="text" align="center" placeholder="Username">
+        <input class="text-input" type="password" align="center" placeholder="Password">
+        <a class="submit" align="center">Sign in</a>
+        <p class="forgot" align="center"><a href="#">Forgot Password?</p>
+    </form>
+</div>
+</body>
+</html>
