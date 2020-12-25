3129c2fd295df307c835054249903a5705ab4316
Granit Bajraktari <41693776+granitba@users.noreply.github.com>
2020-12-25 21:30:40 +0100

    Rregullimi i validimmit ne form

--- login.php
+++ login.php
@@ -6,12 +6,14 @@
 <body id="login-body">
 <div id="login">
     <p class="sign" align="center">Sign in</p>
-    <form class="login-form">
-        <input class="text-input" type="text" align="center" placeholder="Username">
-        <input class="text-input" type="password" align="center" placeholder="Password">
-        <a class="submit" align="center">Sign in</a>
+    <form class="login-form" name="login-form" onsubmit="return formValidation()" method="post">
+        <input class="text-input" type="text" align="center" placeholder="Email" name="email">
+        <input class="text-input" type="password" align="center" placeholder="Password" name="password">
+        <p style="color:red" id="error" align="center"></p>
+        <input class="submit" type="submit" value="Sign in" align="center">
         <p class="forgot" align="center"><a href="#">Forgot Password?</p>
     </form>
 </div>
 </body>
+<script src="assets/js/main.js"></script>
 </html>
